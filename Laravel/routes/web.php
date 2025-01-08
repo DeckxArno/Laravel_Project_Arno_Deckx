b236c626ea236f4c28109ebda8d34eafa1d19c1d
DeckxArno <arno.deckx@student.ehb.be>
2025-01-08 19:28:23 +0100

    configureren database

--- Laravel/routes/web.php
+++ Laravel/routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/events', [EventController::class, 'index']);
+Route::get('/events/{id}', [EventController::class, 'show']);
+Route::post('/events', [EventController::class, 'store']);
+Route::put('/events/{id}', [EventController::class, 'update']);
+Route::delete('/events/{id}', [EventController::class, 'destroy']);
+
